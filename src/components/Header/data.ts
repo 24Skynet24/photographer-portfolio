da317b8e2f9aacd9c3eca2e0fa105afc6f381581
albert.mnac <albert.mnac@gmail.com>
2024-06-11 18:05:55 +0400

    add header

---
+++ src/components/Header/data.ts
@@ -0,0 +1,23 @@
+type navLink = {
+    link: string,
+    title: string,
+}
+
+export const navLinks: navLink[] = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "Book",
+        link: "/book",
+    },
+    {
+        title: "Contacts",
+        link: "/contacts",
+    },
+    {
+        title: "About",
+        link: "/about",
+    },
+]
